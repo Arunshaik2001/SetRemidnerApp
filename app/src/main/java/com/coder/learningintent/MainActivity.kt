dd0d5dcb0473e35ef8bd066e95f103b62b236e4c
Arunshaik2001 <arunshaik432@gmail.com>
2022-11-06 20:28:10 +0530

    set reminders

---
+++ app/src/main/java/com/coder/learningintent/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.coder.learningintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.coder.learningintent.ui.theme.LearningIntentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearningIntentTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ReminderEditScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LearningIntentTheme {
+        Greeting("Android")
+    }
+}
